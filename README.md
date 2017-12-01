fa46a45c5192dd61ce77249e9784c9655cdd97d5
Adrian <adrianbiba1998@gmail.com>
2017-12-01 18:45:40 +0100

    page

---
+++ README.md
@@ -0,0 +1,1 @@
+# deshtaku
--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,35 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "526px";
+    document.getElementById("mySidenav").style.width = "12%";
+    document.getElementById("content").style.marginLeft = "50px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
+
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("content").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
 }
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+
 
 
